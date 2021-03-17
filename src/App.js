d26d56606788aeb560c05c6006ae6e9e1787e2be
Barnes <alexis.barnes06@gmail.com>
2021-03-17 15:00:28 -0400

    Added upload and banner color.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import Grid from '@material-ui/core/Grid';
+import Upload from './components/Upload';
 
 function App() {
   return (
     <div className="main-container">
       <Grid container spacing={3}>
-        <Grid item xs={12}>
+        {/* Page header */}
+        <Grid className="banner" item xs={12}>
           <h1 className="page-title">Update GEAR UP Colleges Data</h1>
           <p className="page-info">Navigate to each file below to update the college info. The clear button clears the page, so proceed with caution.</p>
         </Grid>
+        {/* Upload UX_INS file */}
         <Grid item xs={12}>
           <h2>Update UX_INS:</h2>
           <p>Navigate to the UX_INS.csv file to update the college info.</p>
-          
+          <Upload />
         </Grid>
       </Grid>
     </div>
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,4 +1,35 @@
 import React from 'react';
+import '../Upload.css';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+class Upload extends React.Component {
+    render() {
+        return (
+            <div className="upload">
+                <Grid container spacing={0}>
+                    <Grid item xs={8}>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Upload file"
+                        variant="outlined"
+                        fullWidth={true}
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button variant="contained">Upload</Button>
+                    </Grid>
+                    
+                    
+                </Grid>
+                
+            </div>
+            
+            
+        );
+    }
+}
+
+export default Upload;
