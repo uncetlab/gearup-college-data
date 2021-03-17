030cc6ee29e1ff84fadf955b2e279fe68755e8a5
Barnes <alexis.barnes06@gmail.com>
2021-03-17 14:34:34 -0400

    Home page styles updated. Created upload component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <h1 className="page-title">Update GEAR UP Colleges Data</h1>
+          <p className="page-info">Navigate to each file below to update the college info. The clear button clears the page, so proceed with caution.</p>
+        </Grid>
+        <Grid item xs={12}>
+          <h2>Update UX_INS:</h2>
+          <p>Navigate to the UX_INS.csv file to update the college info.</p>
+          
+        </Grid>
+      </Grid>
     </div>
   );
 }
