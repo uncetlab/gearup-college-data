df25fe5bf37f45f3ce0c229ecb89985408dfe8e7
Barnes <alexis.barnes06@gmail.com>
2021-03-23 16:22:55 -0400

    Added uploader.

--- app/src/components/Upload.js
+++ app/src/components/Upload.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Upload.css';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 const Upload = props => {
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    const changeHandler = (event) => {
+        setSelectedFile(event.target.files[0]);
+        setSelectedFile(true);
+    }
+
+    const handleSubmission = () => {
+        console.log(selectedFile);
+    }
+
     return (
         <div className="upload">
             <Grid container spacing={0}>
                 <Grid item xs={8}>
-                    <TextField
-                    required
-                    id="outlined-required"
-                    label="Upload file"
-                    variant="outlined"
-                    fullWidth={true}
-                    />
                 </Grid>
                 <Grid item xs={4}>
-                    <form>
-                        <Button variant="contained" type="submit" >Upload</Button>
-                    </form>
+                    <input type="file" name="file" onChange={changeHandler} />
+                    <Button variant="contained" type="submit" onClick={handleSubmission}>
+                        Submit
+                    </Button>
                 </Grid>
                 
                 
