be7d985b83f50c88276b4d46857a3fba05b82a7b
Barnes <alexis.barnes06@gmail.com>
2021-04-29 14:04:58 -0400

    Updating code base.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+// Google apis tutorial: https://www.youtube.com/watch?v=PFJNJQCU_lo&t=1224s
+
+const express = require('express')
+const router = express.Router()
+const { google } = require('googleapis')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', async (req, res) => {
+  // run npm i googleapis@latest to fix GoogleAuth is not a constructor error
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: 'https://www.gogleapis.come/auth/spreadsheets'
+  })
 
-module.exports = router;
+  const client = await auth.getClient()
+
+  const googleSheets = google.sheets({ version: 'v4', auth: client })
+
+  // put the id of the spreadsheet
+  const spreadsheetId = ''
+
+  const metadata = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId
+  })
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:A" // read first column
+  })
+
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: 'Sheet1!A:B', // write to first two columns
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [
+        ["Make a tutorial", "test"]
+      ]
+    }
+  })
+  res.send(getRows.data)
+})
+
+module.exports = router
